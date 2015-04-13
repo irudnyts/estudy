dda8ef05eb156ecef068cea67dd25c375e2a3eea
irudnyts <yegor.rudnytskiy@gmail.com>
2015-04-13 15:48:35 +0200

    Add fake comments for GetNextDate

--- R/check_event.R
+++ R/check_event.R
@@ -1,15 +1,27 @@
-#' The clothest date in a given set, which is greater then arument date.
-#' 
-#' Return the closest date in set, which is greater then argument date.
-#' 
-#'  @param date the date, for which the closest will be choosen
-#'  @param set the vector, which contains dates for search
-#'  @return the closest date from set
-#'  @examples
-#'  GetNextDate(as.Date("25.08.2005","%d.%m.%Y"), 
-#'  c(as.Date("25.08.2001","%d.%m.%Y"), 
-#'  as.Date("25.08.2002","%d.%m.%Y"),
-#'  as.Date("25.08.20010","%d.%m.%Y")))
+#' Sum of vector elements.
+#'
+#' \code{sum} returns the sum of all the values present in its arguments.
+#'
+#' This is a generic function: methods can be defined for it directly
+#' or via the \code{\link{Summary}} group generic. For this to work properly,
+#' the arguments \code{...} should be unnamed, and dispatch is on the
+#' first argument.
+#'
+#' @param ... Numeric, complex, or logical vectors.
+#' @param na.rm A logical scalar. Should missing values (including NaN)
+#'   be removed?
+#' @return If all inputs are integer and logical, then the output
+#'   will be an integer. If integer overflow
+#'   \url{http://en.wikipedia.org/wiki/Integer_overflow} occurs, the output
+#'   will be NA with a warning. Otherwise it will be a length-one numeric or
+#'   complex vector.
+#'
+#'   Zero-length vectors have sum 0 by definition. See
+#'   \url{http://en.wikipedia.org/wiki/Empty_sum} for more details.
+#' @examples
+#' sum(1:10)
+#' sum(1:5, 6:10)
+#' sum(F, F, F, T, T)
 GetNextDate <- function(date, set) {
     # Return the closest date in set, which is greater then argument date. 
     # Args:
