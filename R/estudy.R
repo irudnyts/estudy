ffe0090067d4d83572e62597510ca2eca2c04603
irudnyts <yegor.rudnytskiy@gmail.com>
2015-04-13 15:29:28 +0200

    Change documentations in estudy.R

--- R/estudy.R
+++ R/estudy.R
@@ -1,18 +1,20 @@
 #' estudy: A package, which provides functions for event study, approach
-#' described in approach papers by S.J. Brown, J.B. Warner
 #'
 #' The package provides four catigories of basic functions: CleanSeries,
 #' GetRates, CombineZoo and CheckEvent. 
 #' 
 #' @section CleanSeries functions:
 #' Bla-bla-bla about CleanSeries
-#' @section GetRates
+#' 
+#' @section GetRates functions:
 #' Bla-bla-bla about GetRates
-#' @section CombineZoo
+#' 
+#' @section CombineZoo functions:
 #' Bla-bla-bla Ck/fm
-#' @section CheckEvent
+#' 
+#' @section CheckEvent functions:
 #' bla- bl
-#'
+#' 
 #' @docType package
 #' @name estudy
 NULL
