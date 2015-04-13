d8f4c68717a17d6bd4f413d05cc951ba843412a7
irudnyts <yegor.rudnytskiy@gmail.com>
2015-04-13 15:14:38 +0200

    Create file estudy.R for package description. Add main fields and sections

---
+++ R/estudy.R
@@ -0,0 +1,18 @@
+#' estudy: A package, which provides functions for event study, approach
+#' described in approach papers by S.J. Brown, J.B. Warner
+#'
+#' The package provides four catigories of basic functions: CleanSeries,
+#' GetRates, CombineZoo and CheckEvent. 
+#' 
+#' @section CleanSeries functions:
+#' Bla-bla-bla about CleanSeries
+#' @section GetRates
+#' Bla-bla-bla about GetRates
+#' @section CombineZoo
+#' Bla-bla-bla Ck/fm
+#' @section CheckEvent
+#' bla- bl
+#'
+#' @docType package
+#' @name estudy
+NULL
