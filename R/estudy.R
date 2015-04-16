72c971cc92d1ac8725f93b5fbd8e50694c132442
irudnyts <yegor.rudnytskiy@gmail.com>
2015-04-16 11:05:43 +0200

    Add dataset "rates" and roxyhen comments (documentation)

--- R/estudy.R
+++ R/estudy.R
@@ -18,3 +18,30 @@
 #' @docType package
 #' @name estudy
 NULL
+
+#' Dataset of 3'263 daily rates for listed insurance companies and index SXW1E
+#'
+#' Dataset contains 3'263 daily rates from 4.1.2000 to 12.31.2014 for insurance
+#'   companies. Tickers of the companies represent names of the columns:
+#'
+#' \itemize{
+#'   \item date: dates of correspondent rates from "200-01-04" to "2014-12-31"
+#'   \item 000810.KS: Samsung Fire & Marine Insurance Co., Ltd.
+#'   \item AIG: American International Group, Inc.
+#'   \item ALL: The Allstate Corporation
+#'   \item ALV.DE: Allianz SE
+#'   \item BRK-A: Berkshire Hathaway Inc.
+#'   \item CS.PA: Axa SA
+#'   \item G.MI: Assicurazioni Generali S.p.A.
+#'   \item HNR1.HA: Hannover Rück SE
+#'   \item MUV2.DE: Münchener Rückversicherungs-Gesellschaft Aktiengesellschaft
+#'   \item TKOMY: Tokio Marine Holdings Inc.
+#'   \item SXW1E: index STOXX Global 1800
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name rates
+#' @usage data(rates)
+#' @format A data frame with 3263 rows and 12 variables
+NULL
