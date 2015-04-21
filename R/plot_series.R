649404b4069b0a93998d3840d2782e6cdc9b8e43
irudnyts <yegor.rudnytskiy@gmail.com>
2015-04-21 10:18:06 +0200

    Add PlotSeries to highlight weekends + documentations

---
+++ R/plot_series.R
@@ -0,0 +1,23 @@
+#' Plot series with hatched weekends
+#'
+#' Plot series with hatched in gray weekends
+#' 
+#' \code{series} should be data.frame with column "date" 
+#' 
+#' @param series data.frame, with two columns, one is "date".
+#' @return NULL
+#' @examples
+#' PlotSeries(rates[, c(1, 2)])
+PlotSeries <- function(series) {
+    plot(series, type = "o")
+    for(i in 1:length(seq(from = as.Date(series[1, "date"]),
+                          to = as.Date(series[nrow(series), "date"]),
+                          by = 1))) {
+        date <- as.Date(series[1, "date"]) + i
+        if(weekdays(date) == "Friday") {
+            rect(xleft = date, ybottom = par("usr")[3], xright = date + 2,
+                 ytop = par("usr")[4], density = 20, angle = 45,
+                 border = NULL, col = "gray")
+        }
+    }
+}
